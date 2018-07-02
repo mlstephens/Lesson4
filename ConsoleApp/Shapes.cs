eece2ecafa0dab60046224419ccc2725a852ac93
Michael L. Stephens <mlstephens@.zrtlab.com>
2018-07-02 15:21:37 -0700

    Added shapes.cs to consoleapp.

---
+++ ConsoleApp/Shapes.cs
@@ -0,0 +1,28 @@
+﻿using Shape.Circle;
+using Shape.Parallelogram;
+using Shape.Square;
+using Shape.Triangle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Shapes<T>
+    {
+        private T _tValue;
+
+        public T ID { get; set; }
+
+        public List<Circle<T>> Circles { get; set; }
+
+        public List<Parallelogram<T>> Parallelograms { get; set; }
+
+        public List<Square<T>> Squares { get; set; }
+
+        public List<Triangle<T>> Triangles { get; set; }
+
+    }
+}
